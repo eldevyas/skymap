b9ab971024b012e800cda0212dd79eacd7ee41f6
eldevyas <yassinechettouch@gmail.com>
2023-10-03 02:41:02 +0100

    Enahnced API Queries.

--- app/api/cities/search/route.ts
+++ app/api/cities/search/route.ts
@@ -18,75 +18,129 @@ export async function GET(request: Request) {
 
     console.log("Search Query is: ", searchQuery);
 
-    const Cities: CityType[] = await axios
-        .get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${googleMapsAPIKey}&input=${searchQuery}`)
-        .then(async (response) => {
-            const Predictions = response.data.predictions.filter((prediction: any) => {
-                return prediction.types.includes('locality') || prediction.types.includes('administrative_area_level_1') || prediction.types.includes('country');
+    // Check if the query is a country or a place
+    const isCountryOrPlace = !searchQuery || searchQuery.length <= 3;
+
+    let Cities: CityType[] = [];
+
+    if (isCountryOrPlace) {
+        // Handle queries for countries or places
+        Cities = await axios
+            .get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${googleMapsAPIKey}&input=${searchQuery}&types=(country|establishment)`)
+            .then(async (response) => {
+                const Predictions = response.data.predictions;
+
+                console.log(`Found ${Predictions.length} predictions from Google Maps API.`);
+
+                async function fetchCityDetails(prediction: any) {
+                    const responsePredictions = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?key=${googleMapsAPIKey}&place_id=${prediction.place_id}`)
+                        .then(
+                            (response) => {
+                                console.log("Response from Google Maps API is: ", response);
+                                return response.data;
+                            }
+                        ).catch((error) => {
+                            console.error("Error fetching city details from Google Maps API: ", error)
+                            return [];
+                        });
+
+                    const { lat, lng } = responsePredictions.result.geometry.location;
+
+                    // Find the timezone of the city
+                    const timeZone = find(lat, lng)[0];
+
+                    const countryCode = responsePredictions
+                        .result
+                        .address_components
+                        .find((component: {
+                            types: string | string[];
+                        }) => component.types.includes('country'))
+                        .short_name;
+
+                    const predictedCity = {
+                        id: prediction.place_id,
+                        mainText: prediction.structured_formatting.main_text,
+                        secondaryText: prediction.structured_formatting.secondary_text,
+                        countryCode: countryCode,
+                        latitude: lat,
+                        longitude: lng,
+                        timeZone: timeZone,
+                    };
+
+                    console.log("Predicted City is: ", predictedCity);
+
+                    return predictedCity;
+                }
+
+                Cities = await Promise.all(Predictions.map(fetchCityDetails));
+
+                return Cities;
+            })
+            .catch((error) => {
+                console.error("Error fetching cities from Google Maps API", error)
+                return [];
             });
+    } else {
+        // Handle queries for cities
+        Cities = await axios
+            .get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${googleMapsAPIKey}&input=${searchQuery}`)
+            .then(async (response) => {
+                const Predictions = response.data.predictions.filter((prediction: any) => {
+                    return prediction.types.includes('locality') || prediction.types.includes('administrative_area_level_1') || prediction.types.includes('country');
+                });
 
-            console.log(`Found ${Predictions.length} predictions from Google Maps API.`)
+                console.log(`Found ${Predictions.length} predictions from Google Maps API.`);
 
-            // Wrap the map function in another async function to wait for the promises to resolve before accessing the Cities array.
-            async function fetchCityDetails(prediction: any) {
-                const responsePredictions = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?key=${googleMapsAPIKey}&place_id=${prediction.place_id}`)
-                    .then(
-                        (response) => {
-                            console.log("Response from Google Maps API is: ", response);
-                            return response.data;
-                        }
-                    ).catch((error) => {
-                        console.error("Error fetching city details from Google Maps API: ", error)
-                        return [];
-                    });
+                async function fetchCityDetails(prediction: any) {
+                    const responsePredictions = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?key=${googleMapsAPIKey}&place_id=${prediction.place_id}`)
+                        .then(
+                            (response) => {
+                                console.log("Response from Google Maps API is: ", response);
+                                return response.data;
+                            }
+                        ).catch((error) => {
+                            console.error("Error fetching city details from Google Maps API: ", error)
+                            return [];
+                        });
 
-                const { lat, lng } = responsePredictions.result.geometry.location;
+                    const { lat, lng } = responsePredictions.result.geometry.location;
 
-                // Find the timezone of the city
-                const timeZone = find(lat, lng)[0];
+                    // Find the timezone of the city
+                    const timeZone = find(lat, lng)[0];
 
-                const countryCode = responsePredictions
-                    .result
-                    .address_components
-                    .find((component: {
-                        types: string | string[];
-                    }) => component.types.includes('country'))
-                    .short_name;
+                    const countryCode = responsePredictions
+                        .result
+                        .address_components
+                        .find((component: {
+                            types: string | string[];
+                        }) => component.types.includes('country'))
+                        .short_name;
 
-                const predictedCity = {
-                    id: prediction.place_id,
-                    mainText: prediction.structured_formatting.main_text,
-                    secondaryText: prediction.structured_formatting.secondary_text,
-                    countryCode: countryCode,
-                    latitude: lat,
-                    longitude: lng,
-                    timeZone: timeZone,
-                };
+                    const predictedCity = {
+                        id: prediction.place_id,
+                        mainText: prediction.structured_formatting.main_text,
+                        secondaryText: prediction.structured_formatting.secondary_text,
+                        countryCode: countryCode,
+                        latitude: lat,
+                        longitude: lng,
+                        timeZone: timeZone,
+                    };
 
-                console.log("Predicted City is: ", predictedCity);
+                    console.log("Predicted City is: ", predictedCity);
 
-                return predictedCity;
-            }
+                    return predictedCity;
+                }
 
-            const Cities = await Promise.all(Predictions.map(fetchCityDetails));
+                Cities = await Promise.all(Predictions.map(fetchCityDetails));
 
-            return Cities;
-        })
-        .catch((error) => {
-            console.error("Error fetching cities from Google Maps API", error)
-            return [];
-        });
+                return Cities;
+            })
+            .catch((error) => {
+                console.error("Error fetching cities from Google Maps API", error)
+                return [];
+            });
+    }
 
-    // // Return the Cities from the Fuse Search if the search query is not empty.
-    // const searchWithFuse = new Fuse(Cities, {
-    //     keys: ['mainText', 'secondaryText', 'countryCode'],
-    //     threshold: 0.5,
-    // });
-
-    // const searchResults = searchQuery ? searchWithFuse.search(searchQuery) : searchWithFuse.search('');
-
-    // // Convert the Fuse Search Results to an Array of Cities.
-    // const filteredCitiesResult = searchResults.map((result) => result.item);
 
     return new Response(JSON.stringify(Cities), {
         headers: {
