2d4b4c321f5792ed46f2b13ac338b54023079495
eldevyas <yassinechettouch@gmail.com>
2023-10-07 18:11:24 +0100

    🔐 Implementation for Third-party OAuth 2.0.

--- app/lib/authOptions.ts
+++ app/lib/authOptions.ts
@@ -20,6 +20,13 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
         }),
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID ?? "",
